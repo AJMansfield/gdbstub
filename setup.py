9fa0bfb6f46dde46be1559473f65de407941ee44
Anson Mansfield <anson.mansfield@gmail.com>
2025-09-05 12:52:40 -0400

    project: Update project information.

--- setup.py
+++ setup.py
@@ -2,11 +2,18 @@ from distutils.core import setup
 
 
 setup(
-    name="gdb-stubs",
-    url="https://github.com/AJMansfield/gdb-stubs",
+    name="gdbstub",
+    url="https://github.com/AJMansfield/gdbstub",
     author="Anson Mansfield",
     author_email="amansfield@mantaro.com",
     version="0.1",
     package_data={"_gdb": ['__init__.pyi']},
-    packages=["_gdb"]
+    packages=["_gdb"],
+    license="GPL-2.0",
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "Operating System :: OS Independent",
+        "Topic :: Software Development :: Debuggers",
+        "Typing :: Stubs Only",
+    ]
 )
