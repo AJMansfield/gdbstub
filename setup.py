2d7f8197d1a3ef42132ad22fe92b3d1050c37761
Anson Mansfield <anson.mansfield@gmail.com>
2025-09-05 13:20:22 -0400

    project: Calculate package version based on git tags.

--- setup.py
+++ setup.py
@@ -1,12 +1,22 @@
 from distutils.core import setup
 
+def calc_git_version():
+    import subprocess
+    import os
+    try:
+        version = subprocess.run(['git', 'describe', '--tags'], check=True, capture_output=True, encoding='utf-8').stdout
+        assert version[0] == "v"
+        version = version.strip().removeprefix("v").replace("-",".post",1).replace("-","+")
+    except subprocess.CalledProcessError as e:
+        version = os.path.basename(os.path.dirname(__file__)).split("-", maxsplit=1)[1]
+    return version
 
 setup(
     name="gdbstub",
     url="https://github.com/AJMansfield/gdbstub",
     author="Anson Mansfield",
     author_email="amansfield@mantaro.com",
-    version="0.1",
+    version=calc_git_version(),
     package_data={"_gdb": ['__init__.pyi']},
     packages=["_gdb"],
     license="GPL-2.0",
